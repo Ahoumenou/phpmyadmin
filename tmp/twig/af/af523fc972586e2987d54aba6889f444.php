bdeba9a1db6322debcef9f20bfc047a7a38d2f48
Ahoumenou Emile <ahoumenouemile022@gmail.com>
2023-10-20 18:33:18 +0100

    php myadmin

---
+++ tmp/twig/af/af523fc972586e2987d54aba6889f444.php
@@ -0,0 +1,55 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* config/form_display/form_bottom.twig */
+class __TwigTemplate_2ba4a6e1c48ddff0913e8fe4e9f16357 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "</form>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "config/form_display/form_bottom.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "config/form_display/form_bottom.twig", "C:\\_programmes\\laragon-6.0.0\\etc\\apps\\phpmyadmin\\templates\\config\\form_display\\form_bottom.twig");
+    }
+}
